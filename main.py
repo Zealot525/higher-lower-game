6b6b76b6a4ce68acf5a4b38b94007264644578af
Jonathan Chin <132028541+Zealot525@users.noreply.github.com>
2024-03-07 02:28:30 +0000

    comparing A and B

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ choice_A = data[choice_1]
 choice_2 = random.randint(0, len(data) - 1)
 choice_B = data[choice_2]
 
+# comparing A to B
+def comparing(A, B):
+    if choice_A['follower_count'] > choice_B['follower_count']:
+        return "A"
+    elif choice_A['follower_count'] < choice_B['follower_count']:
+        return "B"
+
 # displaying the two list
 print(f"Compare A: {choice_A['name']}, a {choice_A['description'].lower()}, from {choice_A['country']}")
 print(f"Against B: {choice_B['name']}, a {choice_B['description'].lower()}, from {choice_B['country']}")
+print(f"A: {choice_A['follower_count']}, B: {choice_B['follower_count']}")
+
+# asking the user to select the option
+user_choice = input("Who has more followers on Instagram? Type 'A' or 'B': ")
+
+# comparing user choice to the correct answer
+if user_choice == comparing(choice_A['follower_count'], choice_B['follower_count']):
+    print("You got it!")
+else:
+    print("You lose!")
